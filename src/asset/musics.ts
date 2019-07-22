4930288a42ba5b39769047df1e907a45809c4ea6
GQSM <ms314006@hotmail.com>
2019-07-22 22:45:38 +0800

    [ADD] 增加幾首音樂到 musics 中

---
+++ src/asset/musics.ts
@@ -0,0 +1,12 @@
+import { IMusic } from '../lib/reference/IMusic';
+
+const musics: IMusic[] = [
+  { id: '0', name: 'Spy Suite', },
+  { id: '1', name: 'Central Park', },
+  { id: '2', name: 'Ice Cream', },
+  { id: '3', name: 'Keys to the Apocalypse', },
+  { id: '4', name: 'Space Hunter', },
+  { id: '5', name: 'The End', }
+];
+
+export default musics;
