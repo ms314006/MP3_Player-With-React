74b66e683b1649da84ad807e9f5ee7e1d8670caa
GQSM <ms314006@hotmail.com>
2019-07-24 23:26:50 +0800

    [ADD] 把 store 加入最上層 component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import Main from './component/Main';
+import store from './store';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store} >
+    <Main />
+  </Provider>,
+  document.getElementById('root')
+);
