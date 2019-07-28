fc685161322a6487a029bc4ffc8cb76bbef90fc0
GQSM <ms314006@hotmail.com>
2019-07-28 17:25:23 +0800

    [ADD] 替 Music 增加作者的資料

--- src/asset/musics.ts
+++ src/asset/musics.ts
@@ -1,10 +1,10 @@
 import { IMusic } from '../lib/interface/IMusic';
 
 const musics: IMusic[] = [
-  { id: '0', name: 'Images_of_Tomorrow', },
-  { id: '1', name: 'Jungle_Intrigue', },
-  { id: '2', name: 'Keys_to_the_Apocalypse', },
-  { id: '3', name: 'Metamorphosis', }
+  { id: '0', name: 'Images_of_Tomorrow', author: 'Youtube', },
+  { id: '1', name: 'Jungle_Intrigue', author: 'Youtube', },
+  { id: '2', name: 'Keys_to_the_Apocalypse', author: 'Youtube', },
+  { id: '3', name: 'Metamorphosis', author: 'Youtube', }
 ];
 
 export default musics;
--- src/lib/interface/IMusic.ts
+++ src/lib/interface/IMusic.ts
@@ -1,4 +1,5 @@
 export interface IMusic {
   id: string;
   name: string;
+  author: string
 }
