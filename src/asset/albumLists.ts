475750edb3e19088b8c535411e7218374dc4e794
GQSM <ms314006@hotmail.com>
2019-07-28 17:26:26 +0800

    [ADD] 增加專輯和歌單的資料

---
+++ src/asset/albumLists.ts
@@ -0,0 +1,14 @@
+const albums = [
+  {
+    id: '0',
+    name: 'VanillaJS',
+    year: '1995',
+  },
+  {
+    id: '1',
+    name: 'React',
+    year: '2013',
+  }
+];
+
+export default albums;
---
+++ src/asset/musicLists.ts
@@ -0,0 +1,12 @@
+const musicLists = [
+  {
+    id: '0',
+    name: 'My Love',
+  },
+  {
+    id: '1',
+    name: 'Jazzy Dinner',
+  }
+];
+
+export default musicLists;
